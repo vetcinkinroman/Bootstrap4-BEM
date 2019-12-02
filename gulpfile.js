d65e61a093eb13f430777472c1b2c2a38025fbdd
vetcinkinroman <vetcinkinroman@gmail.com>
2019-12-02 20:17:22 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
+// del = require('del'),
+// autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('scss', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        // .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// не создаеться при использовании PUG
+gulp.task('html', function(){
+    return gulp.src('app/**/*.html')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('script', function(){
+    return gulp.src('app/js/*.js')
+    .pipe(browserSync.reload({stream: true}))
+});
+
+
+//js
+gulp.task('js', function(){
+    return gulp.src([
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+    gulp.watch('app/**/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'js', 'browser-sync', 'watch'))
